851a027830a479a136703b0b6d714795939647e0
alvin-odins <alvin.odins@gmail.com>
2016-07-16 16:23:42 -0400

    reomve comments in pethomemanagement

--- src/petzHome/PetHomeManagement.java
+++ src/petzHome/PetHomeManagement.java
@@ -6,15 +6,12 @@ import java.io.InputStreamReader;
 class PetHomeManagement {
 
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    //String newPet[];
-    int front = 0, rear = 0, numberOfPets, count = 0;
-	//String pets;
     
+    int front = 0, rear = 0, numberOfPets, count = 0;    
 	
 	public PetHomeManagement() {
 		int capacity = 20;
 		numberOfPets = capacity;
-		//newPet = new String[numberOfPets];
 	}
 
 }
