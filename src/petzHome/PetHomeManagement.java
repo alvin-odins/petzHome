a395ebcced947ea40c950600dc6ab6dbad62fdb2
alvin-odins <alvin.odins@gmail.com>
2016-07-16 16:17:30 -0400

    add method to register new pet in pets class

--- src/petzHome/PetHomeManagement.java
+++ src/petzHome/PetHomeManagement.java
@@ -17,24 +17,7 @@ class PetHomeManagement {
 		//newPet = new String[numberOfPets];
 	}
 
-    void registerNewPet() {
-        try {
-            if (count < numberOfPets) {
-                System.out.println("Enter the name and breed of dog (e.g Bruno_Alsatian) :");
-                pets = br.readLine();
-                newPet[rear] = pets;
-                int petName = rear++;
-                count++;
-                Thread.sleep(2000);
-                System.out.println("Welcome to PetzHome: "+ newPet[petName]);
-                System.out.println();
-            } else {
-                System.out.println("We are at full capacity now");
-            }
-        } catch (Exception e) {
-            System.out.println("Please verify your inputs");
-        }
-    }
+   
 
     void remove() {
         if (count != 0) {
